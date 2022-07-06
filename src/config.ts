13f9062b5be7243038313f372dd4ff5bf00b2aa9
Harish <harish@cypherock.com>
2022-07-06 16:00:22 +0530

    add v3 sdk version and check

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Supported version from order older to newer
-export const ALL_SUPPORTED_SDK_VERSIONS = ['1.0.0'];
+export const ALL_SUPPORTED_SDK_VERSIONS = ['1.0.0', '2.0.0'];
 
 export const LATEST_SUPPORTED_SDK_VERSION =
   ALL_SUPPORTED_SDK_VERSIONS[ALL_SUPPORTED_SDK_VERSIONS.length - 1];
