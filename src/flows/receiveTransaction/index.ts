dcbc558fbd51799162da7fcbc41ce8c7807cfd01
Tejasv Sharma <tejasvonly@gmail.com>
2022-09-03 12:44:52 +0530

    CSI-1277 passing deferredReference without the mutable reference

--- src/flows/receiveTransaction/index.ts
+++ src/flows/receiveTransaction/index.ts
@@ -408,7 +408,7 @@ export class TransactionReceiver extends CyFlow {
       this.emit('cardTapped');
 
       const waitForUserPromise = async () => {
-        await userAction.current.promise;
+        await userAction.promise;
         stopWaitForAbort = true;
       };
 
@@ -452,7 +452,7 @@ export class TransactionReceiver extends CyFlow {
         this.emit('replaceAccountRequired', true);
 
         const waitForReplaceAccount = async () => {
-          await replaceAccountAction.current.promise;
+          await replaceAccountAction.promise;
           stopWaitForAbort = true;
         };
 
