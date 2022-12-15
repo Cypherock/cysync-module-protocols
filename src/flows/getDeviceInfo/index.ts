319759f43c75ce836231b5b35e4e1716acb79a95
Md Irshad Ansari <irshad@cypherock.com>
2022-12-15 16:06:24 +0530

    fix: sdk incompatible error

--- src/flows/getDeviceInfo/index.ts
+++ src/flows/getDeviceInfo/index.ts
@@ -165,7 +165,7 @@ export class GetDeviceInfo extends CyFlow {
           this.emit('sdkNotSupported', 'device');
         }
 
-        throw new Error('SDK not supported');
+        throw new ExitFlowError();
       }
 
       const packetVersion = connection.getPacketVersion();
